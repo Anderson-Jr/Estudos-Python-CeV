75e1d9db31d466163044f8eee26fb11bd27fcbba
Anderson-Jr <58453796+Anderson-Jr@users.noreply.github.com>
2019-12-16 13:38:07 -0300

    Melhoria no Desafio 073v2

--- Desafio 073v2.py
+++ Desafio 073v2.py
@@ -1,8 +1,6 @@
 times = ('Flamengo', 'Santos', 'Palmeiras', 'Grêmio', 'Athletico', 'São Paulo', 'Internacional', 'Corinthians', 'Fortaleza', 'Goiás', 'Bahia', 'Vasco', 'Atlético-MG', 'Fluminense', 'Botafogo', 'Ceará', 'Cruzeiro', 'CSA', 'Chapecoense', 'Avaí')
-print('Cinco primerios colocados: ', times[0:5])
+print('Cinco primerios colocados: ', times[:5])
 print('Os quatro últimos colocados: ', times[-4:])
 ordem = sorted(times)
 print('Lista dos times em ordem alfebética: ', ordem)
-for pos, time in enumerate(times):
-    if time == 'Chapecoense':
-        print('Chapecoense está na posição: ', pos)
+print('Chapecoense está na posição: ', times.index('Chapecoense'))
