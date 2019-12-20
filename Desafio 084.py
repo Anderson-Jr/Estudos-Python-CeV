c4f531af19a69dac443c6b6764394165e9905ca5
José Anderson <juniordkj90@gmail.com>
2019-12-20 17:40:01 -0300

    Add Desafio 084

--- Desafio 084.py
+++ Desafio 084.py
@@ -1,4 +1,20 @@
-idade = 18
-print(idade)
-idade = 7.5
-print(idade)
+dados = list()
+aux = list()
+conf = 's'
+qt = menorp = maiorp = 0
+while conf.lower() == 's':
+    aux.append(input('Insira o nome da pessoa: '))
+    aux.append(int(input('Insira o peso da pessoa: ')))
+    if qt == 0:
+        menorp = maiorp = aux[-1]
+    if aux[-1] < menorp:
+        menorp = aux[-1]
+    if aux[-1] > maiorp:
+        maiorp = aux[-1]
+    dados.append(aux[:])
+    aux.clear()
+    qt += 1
+    conf = input('Deseja continuar? [S/N]')
+print('Quantidade de pessoas cadastradas: ', qt)
+print('A pessoa mais pesada registrada foi: ', maiorp)
+print('A pessoa mais leve registrada foi: ', menorp)
